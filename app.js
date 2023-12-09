1b9ac51982870b610b917f67d2f03ea6acfbf5f2
suletha <suletha.gopi@gmail.com>
2023-12-09 20:18:29 +0530

    token_added

--- app.js
+++ app.js
@@ -1,28 +1,34 @@
-const path = require("path");
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-
 const sequelize = require("./util/database");
 const userRoutes = require("./routes/user-routes");
 const expenseRoutes = require("./routes/expense-routes");
+const authenticationMiddleware = require("./util/authentication");
+
+const User = require("./models/user");
+const Expense = require("./models/expense");
 
 const app = express();
 
-app.use(bodyParser.json({ extended: false }));
 app.use(cors());
+app.use(bodyParser.json({ extended: false }));
 
 app.use("/users", userRoutes);
-app.use("/", expenseRoutes);
+app.use(authenticationMiddleware);
+app.use(expenseRoutes);
 
+User.hasMany(Expense);
+Expense.belongsTo(User);
+
+// Sync the database models
 sequelize
   .sync()
-  .then((result) => {
-    //console.log(result);
+  .then(() => {
+    // Start the server
     app.listen(3000);
   })
-  .catch((err) => {
-    console.log(err);
+  .catch((error) => {
+    console.error(error);
   });
