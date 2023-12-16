aa7d3bb90b651cdfe79ef5f4eb807f15023eef41
suletha <suletha.gopi@gmail.com>
2023-12-16 07:29:40 +0530

    finalll

--- services/nodemailerservices.js
+++ services/nodemailerservices.js
@@ -1,5 +1,5 @@
 const nodemailer = require("nodemailer");
-//console.log( process.env.NODEMAILER_USER,process.env.NODEMAILER_PASS)
+
 exports.forgetpasswordmail = async (email, uuid) => {
   const transporter = nodemailer.createTransport({
     host: process.env.NODEMAILER_HOST,
@@ -8,27 +8,30 @@ exports.forgetpasswordmail = async (email, uuid) => {
     auth: {
       user: process.env.NODEMAILER_USER,
       pass: process.env.NODEMAILER_PASS,
-
       authMethod: "LOGIN",
     },
+    tls: {
+      ciphers: 'AES128-SHA'
+    }
   });
   const mailConfirm = {
     from: process.env.NODEMAILER_USER,
     to: email,
-    subject: "Change your password using given link",
+    subject: "Change your password using the given link",
     html: `
-          <html>
-            <head>
-            </head>
-            <body>
-              <p>http://localhost:3000/password/resestpassword/${uuid}</p>
-            </body>
-          </html>  `,
+      <html>
+        <head></head>
+        <body>
+          <p>http://localhost:3000/password/resestpassword/${uuid}</p>
+        </body>
+      </html>
+    `,
   };
   try {
     const info = await transporter.sendMail(mailConfirm);
     return info;
   } catch (err) {
-    return "";
+    console.error("Error sending email:", err);
+    return null; // Return null instead of an empty string to signify an error
   }
 };
